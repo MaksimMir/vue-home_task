811b49677e298073631af197354c677d63146cfc
MaxM <mak-miroshnichen@yandex.ru>
2021-08-23 18:21:40 +0300

    vuetify

--- task5/src/main.js
+++ task5/src/main.js
@@ -4,6 +4,7 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import modal from './plugins/ModalWindow'
+import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false;
 Vue.use(modal);
@@ -11,5 +12,6 @@ Vue.use(modal);
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
